a39af93d849c65eaed166a6cd0113d2c847c1c9a
Wilson Pilon <wilsonpilon@outlook.com>
2021-05-20 09:54:37 -0300

    Revert "CLion Adaptado para nova versao"
    This reverts commit 29fbe0cbfb2cf631c286be79a31026eb93fbbb8a.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/barney/System/Dropbox/2021/projetos/msx-tools
-# Build directory: /mnt/c/Users/barney/System/Dropbox/2021/projetos/msx-tools/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
